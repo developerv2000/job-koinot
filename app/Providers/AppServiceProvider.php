d83b4e206d213177c2c961ddaf31171ec095966f
developerv2000 <developerv2000@gmail.com>
2023-11-16 13:20:05 +0500

    Before installing Laravel Breeze

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use App\Support\Helper;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +22,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        View::composer('dashboard.*', function ($view) {
+            $view->with('routeName', Route::currentRouteName())
+                ->with('modelPrefixName', Helper::getModelPrefixName());;
+        });
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,24 @@ Route::controller(VacancyController::class)->name('vacancies.')->group(function 
 Route::controller(ResumeController::class)->name('resumes.')->group(function () {
     Route::post('resumes/store', 'store')->name('store');
 });
+
+// Dashboard Routes
+Route::redirect('/dashboard', '/dashboard/resumes')->middleware('admin');
+
+Route::middleware('admin')->prefix('dashboard')->group(function () {
+    Route::controller(ResumeController::class)->prefix('/resumes')->name('resumes.')->group(function () {
+        Route::get('/', 'dashboardIndex')->name('dashboard.index');
+
+        Route::post('/destroy', 'destroy')->name('destroy');
+    });
+
+    Route::controller(VacancyController::class)->prefix('/vacancies')->name('vacancies.')->group(function () {
+        Route::get('/', 'dashboardIndex')->name('dashboard.index');
+        Route::get('/create', 'create')->name('create');
+        Route::get('/edit/{item}', 'edit')->name('edit');
+
+        Route::post('/store', 'store')->name('store');
+        Route::post('/update', 'update')->name('update');
+        Route::post('/destroy', 'destroy')->name('destroy');
+    });
+});
